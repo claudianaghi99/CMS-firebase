bb3ccd56f6ab276f9f3d0b059c075d5d6afa5dc2
LAPTOP-FFQ4V0J6\Claudia <claudia.naghi@principal33.com>
2021-09-09 22:49:42 +0300

    Added other funcitonalities from previus project

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,7 @@ const firebaseConfig= {
 
 const app= initializeApp(firebaseConfig);
 const db = getFirestore(app);
+var table = document.getElementById("table");
 
 // get all data from firebase
 async function getAllData() {
@@ -73,34 +74,60 @@ function setEventListenerDelete() {
     }
 }
 
-// template to add more easily a document
-
-// await setDoc(doc(db, "employees", "Alexandra"), {
-//   fname: "Alexandra",
-//   lname: "Naghi",
-//   email: "maria@yahoo.com",
-//   sex: "female",
-//   birthday: "11 Septermber 1999",
-// });
-
-// ad an employee
-
+// add an employee
 document.getElementById("submitButton").addEventListener("click", function() {
     var fname_cms = document.getElementById("fname").value;
     var lname_cms = document.getElementById("lname").value;
     var email_cms = document.getElementById("email").value;
     var sex_cms = document.getElementById("sex-list").value;
     var birthday_cms = document.getElementById("birthday").value;
+    var formatBirthday = moment(document.getElementById('birthday').value);
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if(fname_cms == "") {
-        alert("Completati numele");
-    } else {
+
+        if (fname_cms== "") {
+            validate('fname','You must fill this out.');
+            return false;
+        }
+    
+        if (lname_cms == "") {
+            validate('lname','You must fill this out.');
+            return false;
+        }
+    
+        if (email_cms == "") {
+            validate('email','You must fill this out.');
+            return false;
+        }
+    
+        if (sex_cms == "") {
+            validate('sex-list','You must choose an option.');
+            return false;
+        }
+    
+        if (birthday_cms == "") {
+            validate('birthday','You must choose a date.');
+            return false;
+        } 
+    
+        var birthdate = new Date(document.getElementById('birthday').value)
+        if(calculateAge(birthdate) < 16) { 
+            validate_age('birthday');
+            return false;
+        }
+           
+        if(regex.test(String(email_cms).toLowerCase())) {
+        } else {
+            validate('email','Invalid e-mail address.');
+            return false;
+        }
+
     setDoc(doc(db,"employees",`${fname_cms}`), {
         fname: fname_cms,
         lname: lname_cms,
         email: email_cms,
         sex: sex_cms,
-        birthday: birthday_cms
+        birthday: formatBirthday.format("DD MMMM YYYY")
     });
 
     document.getElementById("fname").value = "";
@@ -111,216 +138,72 @@ document.getElementById("submitButton").addEventListener("click", function() {
     
     removeData(table);
     getAllData();
-}});
+;
+});
 
-// clear data 
-document.getElementById("query").addEventListener("change", function() {
-    if(document.getElementById("query").value == "") {
+function calculateAge(birthday_cms) { 
+    var ageDifMs = Date.now() - birthday_cms.getTime();
+    var ageDate = new Date(ageDifMs); 
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+} 
+function validate(inputID, string) {
+    const input = document.getElementById(inputID);
+    const validityState = input.validity;
+  
+    if (validityState.valueMissing) {
+      input.setCustomValidity(string);
+    } 
+    input.reportValidity();
+}
+
+
+
+function validate_age(inputID) {
+    const input = document.getElementById(inputID);
+    input.setCustomValidity("You must be older than 16 years old.");
+    input.reportValidity();
+}
+
+// template to add more easily a document
+
+// await setDoc(doc(db, "employees", ""), {
+//   fname: "",
+//   lname: "Naghi",
+//   email: "maria@yahoo.com",
+//   sex: "female",
+//   birthday: "11 Septermber 1999",
+// });
+
+// sortare dupa nume
+document.getElementById("sort-list").addEventListener("change", async function() {
+    var sortValue = document.getElementById("sort-list").value;
+    if(sortValue == " "){
         removeData(table);
         getAllData();
+    }else
+    if(sortValue == "asc-alphabetically") {
+      removeData(table);
+        const q = query(collection(db, "employees"), orderBy("fname", "asc"));
+        const querySnapshot = await getDocs(q);
+        putData(querySnapshot);
+        console.log("ok");
+    } else {
+        removeData(table);
+       // getAllData();
+        if(sortValue=="desc-alphabetically"){
+            const q = query(collection(db, "employees"), orderBy("fname", "desc"));
+            const querySnapshot = await getDocs(q);
+            putData(querySnapshot);
+        }
     }
 });
 
-
-
-
-
-
-
-
-
-
-// function getData() {
-//     var str = localStorage.getItem("localData");
-//     if(str!=null) {
-//         arr = JSON.parse(str);
-//     }
-// }
-
-// var arr = new Array();
-// //const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-// function addRow() { //addData
-
-//     getData();
-//     // get input values 
-//     arr.push({
-//         fname : document.getElementById("fname").value,
-//         lname : document.getElementById("lname").value,
-//         email : document.getElementById("email").value,
-//         sex : document.getElementById("sex-list").value,
-//         birthday:  document.getElementById("birthday").value
-//        // formatBirthday : moment(document.getElementById('birthday').value)
-//     });
-    
-//     // if (arr[0].fname== "") {
-//     //     validate('fname','You must fill this out.');
-//     //     return false;
-//     // }
-
-//     // if (arr[1].lname == "") {
-//     //     validate('lname','You must fill this out.');
-//     //     return false;
-//     // }
-
-//     // if (arr[2].email == "") {
-//     //     validate('email','You must fill this out.');
-//     //     return false;
-//     // }
-
-//     // if (arr[3].sex == "") {
-//     //     validate('sex-list','You must choose an option.');
-//     //     return false;
-//     // }
-
-//     // if (arr[4].birthday == "") {
-//     //     validate('birthday','You must choose a date.');
-//     //     return false;
-//     // } 
-
-//     // var birthdate = new Date(document.getElementById('birthday').value)
-//     // if(calculateAge(birthdate) < 16) { 
-//     //     validate_age('birthday');
-//     //     return false;
-//     // }
-       
-//     // if(regex.test(String(email).toLowerCase())) {
-//     // } else {
-//     //     validate('email','Invalid e-mail address.');
-//     //     return false;
-//     // }
-//     localStorage.setItem("localData", JSON.stringify(arr));
-//     location.reload();
-// }
-
-// function loadData(){ //showData
-
-//     getData();
-
-//     var table = document.getElementById('table').getElementsByTagName('tbody')[0];
-//     for(i=0;i<arr.length;i++)
-//       {
-
-//         var row= table.insertRow();
-//         var cell1 = row.insertCell();
-//         var cell2 = row.insertCell();
-//         var cell3 = row.insertCell();
-//         var cell4 = row.insertCell();
-//         var cell5 = row.insertCell();
-//         var cell6 = row.insertCell();
-//         cell1.innerHTML = arr[i].fname;
-//         cell2.innerHTML = arr[i].lname;
-//         cell3.innerHTML = arr[i].email;
-//         cell4.innerHTML = arr[i].sex;
-//         cell5.innerHTML = arr[i].birthday;
-//         cell6.innerHTML = '<input type="button" value="Delete" onclick="deleteRoww();" class="btn btn-danger">';
-//       }
-// }
-// function deleteRoww() {
-//     var index;
-//     var table = document.getElementById('table');
-//     for ( var i = 1; i < table.rows.length; i++) {
-//             table.rows[i].cells[5].onclick = function () {
-//             index = this.parentElement.rowIndex;
-//             table.deleteRow(index);
-//         };
-//     } 
-// }
-// function deleteRoww() {
-
-//     getData();
-
-//     var table = document.getElementById('table');
-
-//     var del = JSON.parse(localStorage.getItem("localData"));
-
-//     for(var i = 0; i < del.length; i++) {
-//         if(del.name == table.rows[i].name) {
-//             arr.splice[i];
-            
-//             localStorage.setItem("localData",JSON.stringify(arr));
-//             getData();
-          
-//         }
-//     }
-    
-// } 
-
-// function deleteRow(employee) {
-//     var result = confirm("Are you sure you want to delete this row?");
-//     if (result) {
-//         var e=employee.parentNode.parentNode;
-//         e.parentNode.removeChild(e);
-//     }
-// }
-
-// function calculateAge(birthday) { 
-//     var ageDifMs = Date.now() - birthday.getTime();
-//     var ageDate = new Date(ageDifMs); 
-//     return Math.abs(ageDate.getUTCFullYear() - 1970);
-// } 
-
-// function searchFunction() {
-//     const searchInput = document.getElementById("search");
-//     const rows = document.querySelectorAll('tbody tr');
-   
-//     searchInput.addEventListener('keyup',function(event) {
-//     const q = event.target.value.toLowerCase();
-//     rows.forEach((row) => {
-//         row.querySelector('td').textContent.toLowerCase().startsWith(q)
-//         ? (row.style.display = 'table-row')
-//         : (row.style.display = 'none');
-//     });
-//     });
-// }
-
-// function sortTable_alphabetically() {
-//     var table, rows, switching, i, x, y, shouldSwitch;
-//     table = document.getElementById("table");
-//     switching = true;
-
-//     while (switching) {
-//       switching = false;
-//       rows = table.rows;
-
-//       for (i = 1; i < (rows.length - 1); i++) {
-      
-//         shouldSwitch = false;
-
-//         x = rows[i].getElementsByTagName("td")[0];
-//         y = rows[i + 1].getElementsByTagName("td")[0];
-   
-//         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-//           shouldSwitch = true;
-//           break;
-//         }
-//       }
-//       if (shouldSwitch) {
-//         rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-//         switching = true;
-//       }
-//     }
-// }
-
-// function selectedOption() {
-//         var option=document.getElementById('sort-list');
-//         if(option.value == "alphabetically"){
-//             sortTable_alphabetically();
-//         }
-// }
-
-// function validate(inputID, string) {
-//     const input = document.getElementById(inputID);
-//     const validityState = input.validity;
-  
-//     if (validityState.valueMissing) {
-//       input.setCustomValidity(string);
-//     } 
-//     input.reportValidity();
-// }
-
-// function validate_age(inputID) {
-//     const input = document.getElementById(inputID);
-//     input.setCustomValidity("You must be older than 16 years old.");
-//     input.reportValidity();
-// }
+// //search
+// document.getElementById("search").addEventListener("mouseout", async function() {
+//     var searchString = document.getElementById("search").value;
+//     removeData(table);
+//     const q = query(collection(db, "employees"), where("fname", "==", searchString));
+//     const querySnapshot = await getDocs(q);
+//     putData(querySnapshot);
+//     //console.log("ok")
+// });
